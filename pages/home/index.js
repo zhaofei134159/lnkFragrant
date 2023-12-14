cf1036be50beaa9e3f2809ecc862657351b13e42
zhaofei <13700530350@163.com>
2023-12-14 18:01:34 +0800

    第一次更新

---
+++ pages/home/index.js
@@ -0,0 +1,43 @@
+// index.js
+Component({
+  data: {
+    appletName: "墨香随笔"
+  },
+  methods: {
+    // 事件处理函数
+    bindViewTap() {
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
+    },
+    onChooseAvatar(e) {
+      const { avatarUrl } = e.detail
+      const { nickName } = this.data.userInfo
+      this.setData({
+        "userInfo.avatarUrl": avatarUrl,
+        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      })
+    },
+    onInputChange(e) {
+      const nickName = e.detail.value
+      const { avatarUrl } = this.data.userInfo
+      this.setData({
+        "userInfo.nickName": nickName,
+        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
+      })
+    },
+    getUserProfile(e) {
+      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      wx.getUserProfile({
+        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+        success: (res) => {
+          console.log(res)
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    },
+  },
+})
