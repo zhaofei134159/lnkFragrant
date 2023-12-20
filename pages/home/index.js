010ecba8d980cf77077a034bc30e1379047b62aa
zhaofei <zhaofei@51smart.com>
2023-12-20 19:22:44 +0800

    修改

--- pages/home/index.js
+++ pages/home/index.js
@@ -15,84 +15,56 @@ Page({
   data: {
     scrollH: 0,
     imgWidth: 0,
-    loadingCount: 0,
-    images: [],
     col1: [],
     col2: []
   },
 
-  // 获取列表
-  onImageLoad: function (e) {
-      let imageId = e.currentTarget.id;
-      let oImgW = e.detail.width;         //图片原始宽度
-      let oImgH = e.detail.height;        //图片原始高度
-      let imgWidth = this.data.imgWidth;  //图片设置的宽度
-      let scale = imgWidth / oImgW;        //比例计算
-      let imgHeight = oImgH * scale;      //自适应高度
+  loadImages: function () {
+      // 获取图片和文章信息
+      let images = [
+        { pic: "/resource/images/bookmark/1.jpg", 'id': '0', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/2.jpg", 'id': '1', 'width': '1200', 'height': '675'},
+        { pic: "/resource/images/bookmark/3.jpg", 'id': '2', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/4.jpg", 'id': '3', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/5.jpg", 'id': '4', 'width': '1200', 'height': '675'},
+        { pic: "/resource/images/bookmark/6.jpeg", 'id': '5', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/7.jpg", 'id': '6', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/8.jpg", 'id': '7', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/9.jpg", 'id': '8', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/10.jpg", 'id': '9', 'width': '1200', 'height': '675'},
+        { pic: "/resource/images/bookmark/11.jpg", 'id': '10', 'width': '420', 'height': '748'},
+        { pic: "/resource/images/bookmark/12.jpg", 'id': '11', 'width': '420', 'height': '748'}      
+      ];
 
-      let images = this.data.images;
-      let imageObj = null;
-
-      for (let i = 0; i < images.length; i++) {
-          let img = images[i];
-          if (img.id === imageId) {
-              imageObj = img;
-              break;
-          }
-      }
-
-      imageObj.height = imgHeight;
-
-      let loadingCount = this.data.loadingCount - 1;
       let col1 = this.data.col1;
       let col2 = this.data.col2;
 
-      console.log(e)
-      console.log('imageId: ' + imageId)
-      console.log('col1H: ' + col1H)
-      console.log('col2H: ' + col2H)
+      for (let i = 0; i < images.length; i++) {
+        let img = images[i];
 
-      if (col1H <= col2H) {
-          col1H += imgHeight;
-          col1.push(imageObj);
-      } else {
-          col2H += imgHeight;
-          col2.push(imageObj);
+        let oImgW = img.width;         //图片原始宽度
+        let oImgH = img.height;        //图片原始高度
+        let imgWidth = this.data.imgWidth;  //图片设置的宽度
+        let scale = imgWidth / oImgW;        //比例计算
+        let imgHeight = oImgH * scale;      //自适应高度
+        let imageObj = null
+        imageObj = img 
+        imageObj.height = imgHeight;
+
+        if (col1H <= col2H) {
+            col1H += imgHeight;
+            col1.push(imageObj);
+        } else {
+            col2H += imgHeight;
+            col2.push(imageObj);
+        }
       }
 
       let data = {
-          loadingCount: loadingCount,
           col1: col1,
           col2: col2
-      };
-
-      if (!loadingCount) {
-          data.images = [];
       }
-
       this.setData(data);
-  },
-
-  loadImages: function () {
-      let images = [
-        { pic: "/resource/images/bookmark/1.jpg", 'id': '0'},
-        { pic: "/resource/images/bookmark/2.jpg", 'id': '1'},
-        { pic: "/resource/images/bookmark/3.jpg", 'id': '2'},
-        { pic: "/resource/images/bookmark/4.jpg", 'id': '3'},
-        { pic: "/resource/images/bookmark/5.jpg", 'id': '4'},
-        { pic: "/resource/images/bookmark/6.jpeg", 'id': '5'},
-        { pic: "/resource/images/bookmark/7.jpg", 'id': '6'},
-        { pic: "/resource/images/bookmark/8.jpg", 'id': '7'},
-        { pic: "/resource/images/bookmark/9.jpg", 'id': '8'},
-        { pic: "/resource/images/bookmark/10.jpg", 'id': '9'},
-        { pic: "/resource/images/bookmark/11.jpg", 'id': '10'},
-        { pic: "/resource/images/bookmark/12.jpg", 'id': '11'}      
-      ];
-
-      this.setData({
-          loadingCount: images.length,
-          images: images
-      });
   },
 
   /**
