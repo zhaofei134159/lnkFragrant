aff71371dd56d3e5f7ad42f52f0d86afbbdd50be
zhaofei <13700530350@163.com>
2023-12-21 02:12:18 +0800

    修改

--- pages/home/index.js
+++ pages/home/index.js
@@ -15,26 +15,15 @@ Page({
   data: {
     scrollH: 0,
     imgWidth: 0,
+    currentPage: 0,
+    pageTotal: 0,
     col1: [],
     col2: []
   },
 
   loadImages: function () {
       // 获取图片和文章信息
-      let images = [
-        { pic: "/resource/images/bookmark/1.jpg", 'id': '0', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/2.jpg", 'id': '1', 'width': '1200', 'height': '675'},
-        { pic: "/resource/images/bookmark/3.jpg", 'id': '2', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/4.jpg", 'id': '3', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/5.jpg", 'id': '4', 'width': '1200', 'height': '675'},
-        { pic: "/resource/images/bookmark/6.jpeg", 'id': '5', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/7.jpg", 'id': '6', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/8.jpg", 'id': '7', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/9.jpg", 'id': '8', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/10.jpg", 'id': '9', 'width': '1200', 'height': '675'},
-        { pic: "/resource/images/bookmark/11.jpg", 'id': '10', 'width': '420', 'height': '748'},
-        { pic: "/resource/images/bookmark/12.jpg", 'id': '11', 'width': '420', 'height': '748'}      
-      ];
+      let images = this.homeData()
 
       let col1 = this.data.col1;
       let col2 = this.data.col2;
@@ -49,7 +38,8 @@ Page({
         let imgHeight = oImgH * scale;      //自适应高度
         let imageObj = null
         imageObj = img 
-        imageObj.height = imgHeight;
+        imageObj.imgHeight = imgHeight;
+        imageObj.bodyHeight = imgHeight + 60;
 
         if (col1H <= col2H) {
             col1H += imgHeight;
@@ -58,6 +48,7 @@ Page({
             col2H += imgHeight;
             col2.push(imageObj);
         }
+
       }
 
       let data = {
@@ -77,7 +68,7 @@ Page({
         let ww = res.windowWidth;
         let wh = res.windowHeight;
         let imgWidth = ww * 0.48;
-        let scrollH = wh;
+        let scrollH = wh + 100;
 
         this.setData({
             scrollH: scrollH,
@@ -87,6 +78,47 @@ Page({
       }
     })
 
+  },
+
+  homeData: function () {
+    var currentPage = parseInt(this.data.currentPage) + 1;
+
+    var result = {
+      images: [
+        { pic: "/resource/images/bookmark/1.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德", 'title': '古装风景图古装风景图古装风景图古装风景图古装风景图装风景图装风景图装风景图', 'id': '0', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/2.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图装风景图', 'id': '1', 'width': '1200', 'height': '675', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/3.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德", 'title': '古装风景图古装风景图古装风景图古装风景图古装', 'id': '2', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/4.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景风景图', 'id': '3', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/5.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '4', 'width': '1200', 'height': '675', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/6.jpeg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '5', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/7.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '6', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/8.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '7', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/9.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '8', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/10.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '9', 'width': '1200', 'height': '675', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/11.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图', 'id': '10', 'width': '420', 'height': '748', lookNum: '154.2k'},
+        { pic: "/resource/images/bookmark/12.jpg", content: "", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德",  'title': '古装风景图古装风景图古装风景图古装风景图古装风景图装风景图装风景图', 'id': '11', 'width': '420', 'height': '748', lookNum: '154.2k'},    
+        
+        { pic: "", content: "大叔大婶是的那首的垃圾送点钱你未来的看似达拉斯拉到吉安卡罗打 阿斯顿阿德阿德阿德阿德啊是的请问阿松大请问刷单啊", headPic: "/resource/images/head/1.jpg", userName: "爱仕达多无群二阿萨德", 'title': '古装风景图古装风景图古装风景图古装风景图古装风景图装风景图装风景图', 'id': '12', 'width': '420', 'height': '748', lookNum: '154.2k'}    
+      ],
+      pageTotal: 1,
+      currentPage: currentPage,
+    }
+
+    this.setData({
+        pageTotal: result.pageTotal,
+        currentPage: result.currentPage,
+    });
+
+    let images = result.images
+
+    if(result.currentPage > result.pageTotal){
+      images = []
+    }
+    // console.log('pageTotal: ' + result.pageTotal)
+    // console.log('currentPage: ' + result.currentPage)
+    // console.log(images)
+
+    return images;
   },
 
   /**
@@ -128,7 +160,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    this.getAppList(true);
+    this.loadImages();
   },
 
   /**
